3c169507ce2a50e702d4ee84998cc6b435489eac
Ali Khalil <alikhalil809@gmail.com>
2025-03-11 19:57:55 +0500

    Adjust pattern file to display all AoB scan results. Dev utility

--- pattern.cpp
+++ pattern.cpp
@@ -8,19 +8,49 @@ int main()
   if (game.Attach(L"Polaris-Win64-Shipping.exe"))
   {
     auto start = std::chrono::high_resolution_clock::now(); // Start timer
+    uintptr_t addr = 0;
 
-    // uintptr_t startAddr = game.getBaseAddress() + 0x5A0000;
-    // uintptr_t endAddr = game.getBaseAddress() + 0x5FFFFFF;
-    uintptr_t addr = game.FastAoBScan(Tekken::DEVIL_FLAG_SIG_BYTES);
-    addr = game.readInt32(addr + 3);
+    // Encryption Method
+    addr = game.FastAoBScan(Tekken::ENC_SIG_BYTES);
+    addr -= game.getBaseAddress();
+    printf("Encryption Method Address: 0x%llX\n", addr);
+
+    // HUD Icon
+    addr = game.FastAoBScan(Tekken::HUD_ICON_SIG_BYTES);
+    addr += 13;
+    addr -= game.getBaseAddress();
+    printf("HUD Icon Address: 0x%llX\n", addr);
+
+    // HUD Name
+    addr = game.FastAoBScan(Tekken::HUD_NAME_SIG_BYTES, addr + 0x10, addr + 0x1000);
+    addr += 13;
+    addr -= game.getBaseAddress();
+    printf("HUD Name Address: 0x%llX\n", addr);
+
+    // Moveset Offset
+    addr = game.FastAoBScan(Tekken::MOVSET_OFFSET_SIG_BYTES);
+    // addr = game.readInt32(addr + 3);
+    addr -= game.getBaseAddress();
+    printf("Moveset Offset Address: 0x%llX\n", addr);
+
+    // Devil Flag Offset
+    addr = game.FastAoBScan(Tekken::DEVIL_FLAG_SIG_BYTES);
+    // addr = game.readInt32(addr + 3);
+    addr -= game.getBaseAddress();
+    printf("Devil Flag Offset Address: 0x%llX\n", addr);
+
+    // Player Struct Base Address
+    addr = game.FastAoBScan(Tekken::PLAYER_STRUCT_SIG_BYTES);
+    // addr = addr + 7 + game.readUInt32(addr + 3);
+    addr -= game.getBaseAddress();
+    printf("Player Struct Base Address: 0x%llX\n", addr);
 
     auto end = std::chrono::high_resolution_clock::now(); // End timer
 
     // Calculate elapsed time in milliseconds
     std::chrono::duration<double, std::milli> elapsed = end - start;
 
-    printf("Address: 0x%llx\n", addr);
-    std::cout << "Time taken: " << elapsed.count() << " ms" << std::endl;
+    std::cout << "Time taken: " << elapsed.count() / 1000.0 << " s" << std::endl;
   }
   return 0;
 }
