83e5d394e5256778453507fa0b06cb607973b71b
Ali Khalil <alikhalil809@gmail.com>
2025-03-11 21:11:57 +0500

    Optimize the pattern script

--- pattern.cpp
+++ pattern.cpp
@@ -9,14 +9,28 @@ int main()
   {
     auto start = std::chrono::high_resolution_clock::now(); // Start timer
     uintptr_t addr = 0;
+    uintptr_t startAddr = game.getBaseAddress();
+
+    // Player Struct Base Address
+    addr = game.FastAoBScan(Tekken::PLAYER_STRUCT_SIG_BYTES, startAddr + 0x5A00000);
+    // addr = addr + 7 + game.readUInt32(addr + 3);
+    startAddr = addr;
+    addr -= game.getBaseAddress();
+    printf("Player Struct Base Address: 0x%llX\n", addr);
+
+    // Match Struct Base Address
+    addr = game.FastAoBScan(Tekken::MATCH_STRUCT_SIG_BYTES, startAddr);
+    // addr = addr + 7 + game.readUInt32(addr + 3);
+    addr -= game.getBaseAddress();
+    printf("Match Struct Base Address: 0x%llX\n", addr);
 
     // Encryption Method
-    addr = game.FastAoBScan(Tekken::ENC_SIG_BYTES);
+    addr = game.FastAoBScan(Tekken::ENC_SIG_BYTES, game.getBaseAddress() + 0x1700000);
     addr -= game.getBaseAddress();
     printf("Encryption Method Address: 0x%llX\n", addr);
 
     // HUD Icon
-    addr = game.FastAoBScan(Tekken::HUD_ICON_SIG_BYTES);
+    addr = game.FastAoBScan(Tekken::HUD_ICON_SIG_BYTES, startAddr);
     addr += 13;
     addr -= game.getBaseAddress();
     printf("HUD Icon Address: 0x%llX\n", addr);
@@ -28,22 +42,16 @@ int main()
     printf("HUD Name Address: 0x%llX\n", addr);
 
     // Moveset Offset
-    addr = game.FastAoBScan(Tekken::MOVSET_OFFSET_SIG_BYTES);
+    addr = game.FastAoBScan(Tekken::MOVSET_OFFSET_SIG_BYTES, game.getBaseAddress() + 0x1800000);
     // addr = game.readInt32(addr + 3);
     addr -= game.getBaseAddress();
     printf("Moveset Offset Address: 0x%llX\n", addr);
 
     // Devil Flag Offset
-    addr = game.FastAoBScan(Tekken::DEVIL_FLAG_SIG_BYTES);
+    addr = game.FastAoBScan(Tekken::DEVIL_FLAG_SIG_BYTES, game.getBaseAddress() + 0x2C00000);
     // addr = game.readInt32(addr + 3);
     addr -= game.getBaseAddress();
     printf("Devil Flag Offset Address: 0x%llX\n", addr);
-
-    // Player Struct Base Address
-    addr = game.FastAoBScan(Tekken::PLAYER_STRUCT_SIG_BYTES);
-    // addr = addr + 7 + game.readUInt32(addr + 3);
-    addr -= game.getBaseAddress();
-    printf("Player Struct Base Address: 0x%llX\n", addr);
 
     auto end = std::chrono::high_resolution_clock::now(); // End timer
 
