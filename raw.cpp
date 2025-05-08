eab15dde60909a9be272fc4893f7d231f38f46a2
Ali Khalil <alikhalil809@gmail.com>
2025-05-09 01:46:02 +0500

    modify `getRawMovesetAddress` to also get file size by passing last offset

--- raw.cpp
+++ raw.cpp
@@ -4,12 +4,18 @@
 // This file mainly is for trying to get some meaningful values from the raw moveset file
 // i.e, length of the move name etc...
 
+enum RawFile
+{
+  Address = 0x0,
+  Size = 0x8,
+};
+
 GameClass game;
 
 uintptr_t PLAYER_STRUCT_BASE = 0x09AB9928;
 uintptr_t MOVESET_OFFSET = 0x3538;
 
-uintptr_t getRawMovesetAddress(int side);
+uintptr_t getRawMovesetAddress(int side, DWORD offset);
 uintptr_t getMovesetAddress(uintptr_t playerAddr);
 uintptr_t getPlayerAddress(int side);
 
@@ -18,7 +24,11 @@ int main()
   if (game.Attach(L"Polaris-Win64-Shipping.exe"))
   {
     int player = 0;
-    uintptr_t rawMoveset = getRawMovesetAddress(player);
+    uintptr_t rawMoveset = getRawMovesetAddress(player, RawFile::Address);
+    uintptr_t fileSize = getRawMovesetAddress(player, RawFile::Size);
+    printf("0x%llx %d bytes\n", rawMoveset, fileSize);
+    return 0;
+
     uintptr_t moveset = getMovesetAddress(getPlayerAddress(player));
     uintptr_t movelist = game.readUInt64(moveset + Tekken::Offsets::MovesHeader);
     int count = game.readUInt64(moveset + Tekken::Offsets::MovesCount);
@@ -49,6 +59,12 @@ int main()
 
       int animNameLen = game.readUInt64(count - 1 > i ? off + 0x448 : off) - game.readUInt64(off);
 
+      encrypted = game.read<EncryptedValue>(addr + 0xD0);
+      int ordinalId1 = Tekken::validateAndTransform64BitValue(&encrypted);
+
+      encrypted = game.read<EncryptedValue>(addr + 0xF0);
+      int ordinalId2 = Tekken::validateAndTransform64BitValue(&encrypted);
+
       printf("%-5d ", i);
       printf("0x%.8x ", nameKey);
       printf("0x%.8x ", animKey);
@@ -56,6 +72,8 @@ int main()
       // printf("%s ", animRawIdx == i ? "Yes" : "No");
       printf("%-5d ", nameLen - 1);
       printf("%-5d ", animNameLen - 1);
+      printf("0x%.8x ", ordinalId1);
+      printf("0x%.8x ", ordinalId2);
       printf("\n");
       // if (i >= 11) break;
     }
@@ -63,10 +81,10 @@ int main()
   return 0;
 }
 
-uintptr_t getRawMovesetAddress(int side)
+uintptr_t getRawMovesetAddress(int side, DWORD offset)
 {
   // TODO: Change first offset in the future
-  return game.getAddress({0x09AB2B08, 0x68, (DWORD)(0x2D0 + side * 0x10), 0x18, 0x8, 0x118, 0x0});
+  return game.getAddress({0x09AB2B08, 0x68, (DWORD)(0x2D0 + side * 0x10), 0x18, 0x8, 0x118, offset});
 }
 
 uintptr_t getPlayerAddress(int side)
